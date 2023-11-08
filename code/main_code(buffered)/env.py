d1da52a3ee5034fd3fed22d5df79a109ab8a1013
DESKTOP-169SJS4\myeongro <leeroy7321@gmail.com>
2023-11-08 12:19:53 +0900

    env step method v1

--- code/main_code(buffered)/env.py
+++ code/main_code(buffered)/env.py
@@ -32,27 +32,27 @@ class JammingEnv:
         
         return self.state
    
-        def step(self, action, time_step):
-            reward = 0
-        self.reset()
+    def step(self, action, time_step):
+        reward = 0
+    self.reset()
 
-        for jammer in self.jammers:
-            self.state[jammer.position] = 1
+    for jammer in self.jammers:
+        self.state[jammer.position] = 1
 
-        if self.state[action] == 1:  # Jammed
-            reward = -1
-        else:
-            reward = 1
+    if self.state[action] == 1:  # Jammed
+        reward = -1
+    else:
+        reward = 1
 
-        if time_step >= self.max_steps:
-            done = True
-        else:
-            done = False
+    if time_step >= self.max_steps:
+        done = True
+    else:
+        done = False
 
-        next_state = self.state.copy()
-        self.observation_space = next_state
+    next_state = self.state.copy()
+    self.observation_space = next_state
 
-        return next_state, reward, done
+    return next_state, reward, done
         
     def render(self):
         """Visualize the current state of the environment."""
